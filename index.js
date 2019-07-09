e192a98dddb075f68539666218373751d217ac54
Pere Zamora <pzamgar@gmail.com>
2019-07-09 23:08:15 +0200

    Add mongo db crud

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const cors = require('cors');
+const cors = require("cors");
+
+require("dotenv").config();
+const Person = require("./models/person");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static('build'))
+app.use(express.static("build"));
 
 morgan.token("body", function(req, res) {
   return JSON.stringify(req.body);
@@ -17,62 +20,32 @@ app.use(
   )
 );
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    phone: "040-123456",
-    id: 1
-  },
-  {
-    name: "Ada Lovelace",
-    phone: "39-44-5323523",
-    id: 2
-  },
-  {
-    name: "Dan Abramov",
-    phone: "12-43-234345",
-    id: 3
-  },
-  {
-    name: "Mary Poppendieck",
-    phone: "39-23-6423122",
-    id: 4
-  }
-];
-
 app.get("/api/info", (req, res) => {
-  let body = `<p>Phonebook has info for ${persons.length} people</p>`;
-  body += `<p>${new Date()}</p>`;
-
-  res.send(body);
+  Person.count({}, (err, count) => {
+    let body = `<p>Phonebook has info for ${count} people</p>`;
+    body += `<p>${new Date()}</p>`;
+    res.send(body);
+  });
 });
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then(persons => {
+    res.json(persons.map(p => p.toJSON()));
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find(p => p.id === id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  Person.findById(request.params.id).then(p => {
+    response.json(p.toJSON());
+  });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter(p => p.id !== id);
-
-  response.status(204).end();
+  Person.findByIdAndRemove(request.params.id, (err, todo) => {
+    if (err) return res.status(500).send(err);
+    response.status(204).end();
+  });
 });
-
-const generateId = () => {
-  const min = Math.ceil(0);
-  const max = Math.floor(999999999);
-  return Math.floor(Math.random() * (max - min)) + min;
-};
 
 const validateBodyPerson = (body, response) => {
   if (!body.name) {
@@ -81,11 +54,6 @@ const validateBodyPerson = (body, response) => {
 
   if (!body.phone) {
     return "phone missing";
-  }
-
-  const person = persons.find(p => p.name === body.name);
-  if (person) {
-    return "name must be unique";
   }
 };
 
@@ -98,16 +66,14 @@ app.post("/api/persons", (request, response) => {
       error: error
     });
   }
-
-  const person = {
+  const person = new Person({
     name: body.name,
-    phone: body.phone,
-    id: generateId()
-  };
+    phone: body.phone
+  });
 
-  persons = persons.concat(person);
-
-  response.json(person);
+  person.save().then(savedPerson => {
+    response.json(savedPerson.toJSON());
+  });
 });
 
 const unknownEndpoint = (request, response) => {
@@ -116,7 +82,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
